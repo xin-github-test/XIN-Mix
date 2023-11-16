76a0f4fc1cfa91cbc512458a632ca14591550ff8
xin <1246814044@qq.com>
2023-11-16 14:20:12 +0800

    开发中....

---
+++ xmix-common/src/test/java/com/xin/xmix/common/XmixCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xin.xmix.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmixCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ xmix-manager/src/test/java/com/xin/xmix/manager/XmixManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xin.xmix.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmixManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
